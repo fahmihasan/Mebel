34ae90d5b61cbc3d572be4bca5b3043fe003a1b3
fahmihasan <fahmi.hasan97@gmail.com>
2018-07-03 07:10:50 +0700

    fahmi

---
+++ application/views/mebel/tambah_user_view.php
@@ -0,0 +1,84 @@
+
+    <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/datatables.min.css">
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Mebel Jepara</title>
+
+    <link href="<?php echo base_url();?>assets/as/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="<?php echo base_url();?>assets/as/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="<?php echo base_url();?>assets/as/stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
+    <link rel="<?php echo base_url();?>assets/as/stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
+    <link rel="<?php echo base_url();?>assets/as/stylesheet" type="text/css" href="assets/lineicons/style.css">    
+    <link href="<?php echo base_url();?>assets/as/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/as/css/style-responsive.css" rel="stylesheet">
+
+    <script src="<?php echo base_url();?>assets/as/js/chart-master/Chart.js"></script>
+    <link rel="icon" type="image/png" href="<?php echo base_url();?>assets/images/icons/favicon.ico"/>
+
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/util.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css">
+<!--===============================================================================================-->
+
+  </head>
+
+  <body>
+
+  <section id="container" >
+     <header class="header black-bg">
+            <a href="index.html" class="logo"><b>Mebel Jepara</b></a>
+            <div class="top-menu">
+              <ul class="nav pull-right top-menu">
+                    <button type="button" class="btn btn-primary btn-md"><li><a class="login" href="<?php echo site_url('');?>">Login</a></li></button>
+              </ul>
+            </div>
+        </header>
+      <div class="container-login100">
+      <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+            
+            <h4><center>Daftar</center></h4>
+            <br><br>
+	
+	<?php echo form_open_multipart('login/create'); ?>
+
+	<?php echo validation_errors();  ?>
+	<div class="form-group">
+		<label for="">Username</label>
+		<input type="text" name="username" class="form-control"  placeholder="Masukkan Username">
+		<label for="">Password</label>
+		<input type="password" name="password" class="form-control"  placeholder="Masukkan Password">
+    <label for="">Hak Akses</label>
+    <input type="hak_akses" name="hak_akses" class="form-control"  placeholder="user / admin">
+	</div>
+	<button type="submit" class="btn btn-primary">OK</button>
+	<?php echo form_close(); ?>
+
+
+    <script src="<?php echo base_url();?>assets/as/js/jquery.js"></script>
+    <script src="<?php echo base_url();?>assets/as/js/jquery-1.8.3.min.js"></script>
+    <script src="<?php echo base_url();?>assets/as/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="<?php echo base_url();?>assets/as/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="<?php echo base_url();?>assets/as/js/jquery.scrollTo.min.js"></script>
+    <script src="<?php echo base_url();?>assets/as/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="<?php echo base_url();?>assets/as/js/jquery.sparkline.js"></script>
+
+
+    <script src="<?php echo base_url();?>assets/as/js/common-scripts.js"></script>
+    
+    <script type="text/javascript" src="<?php echo base_url();?>assets/as/js/gritter/js/jquery.gritter.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/as/js/gritter-conf.js"></script>
+
+
+    <script src="assets/js/sparkline-chart.js"></script>    
+  <script src="assets/js/zabuto_calendar.js"></script>
